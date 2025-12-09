8abc64964c1520f22ef5b89edfaf5d88b108569f
ben <24520152@gm.uit.edu.vn>
2025-12-09 22:08:15 +0700

    Reconfigure postgres

--- src/database.py
+++ src/database.py
@@ -3,8 +3,8 @@ from typing import Generator
 from sqlmodel import create_engine, SQLModel, Session
 
 # Postgres configuration
-HEATMAP_DATABASE = os.environ.get('DATABASE_URL', 'sqlite:///heatmap.db')
-engine = create_engine(url=HEATMAP_DATABASE, echo=False)
+HEATMAP_DATABASE = os.environ.get('DATABASE_URL')
+engine = create_engine(url=HEATMAP_DATABASE, echo=False) # type: ignore
 
 def create_database_and_table():
     SQLModel.metadata.create_all(engine)
