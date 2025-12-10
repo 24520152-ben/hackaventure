99e2075dd467e14224716137633007efbd06a47b
ben <24520152@gm.uit.edu.vn>
2025-12-10 10:21:09 +0700

    Use delete_expired_entires as BackgroundTasks in @router.get('/heatmap')

--- src/heatmap/router.py
+++ src/heatmap/router.py
@@ -1,9 +1,10 @@
 from sqlmodel import Session, select
 from datetime import date, timedelta
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks
 
 from src.database import get_session
 from src.heatmap.models import HeatMap
+from src.heatmap.service import delete_expired_entries
 
 router = APIRouter(tags=['Heatmap'])
 
@@ -18,11 +19,15 @@ def get_unique_products(db: Session = Depends(get_session)):
 @router.get('/heatmap')
 def get_heatmap_by_product(
     product: str,
+    background_tasks: BackgroundTasks,
     horizon: int = 7,
     db: Session = Depends(get_session),
 ):
-    last_day = date.today() + timedelta(days=horizon)
-    statement = select(HeatMap).where(HeatMap.product == product, HeatMap.target_date <= last_day)
+    background_tasks.add_task(delete_expired_entries, db)
+
+    today = date.today()
+    last_day = today + timedelta(days=horizon)
+    statement = select(HeatMap).where(HeatMap.product == product, HeatMap.target_date > today, HeatMap.target_date <= last_day)
     results = db.exec(statement).all()
     return {
         'product': product,
