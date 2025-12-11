edca3179f05a40d38ecf9500bdeca92d91c9086a
ben <24520152@gm.uit.edu.vn>
2025-12-11 16:52:04 +0700

    Modify connection to postgres server

--- src/database.py
+++ src/database.py
@@ -4,7 +4,13 @@ from sqlmodel import create_engine, SQLModel, Session
 
 # Postgres configuration
 HEATMAP_DATABASE = os.environ.get('DATABASE_URL')
-engine = create_engine(url=HEATMAP_DATABASE, echo=False) # type: ignore
+engine = create_engine(
+    url=HEATMAP_DATABASE, # type: ignore
+    pool_pre_ping=True,
+    pool_recycle=300,
+    echo=False,
+    connect_args={'sslmode': 'require'},
+)
 
 def create_database_and_table():
     SQLModel.metadata.create_all(engine)
